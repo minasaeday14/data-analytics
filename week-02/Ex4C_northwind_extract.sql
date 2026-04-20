6dba7c1fd60b089d0677f00aa7ae606f7597c742
Mina Saeday <minaseid14@gmail.com>
2026-04-20 00:59:24 -0500

    Add Ex4C

---
+++ week-02/Ex4C_northwind_extract.sql
@@ -0,0 +1,11 @@
+/*
+a) The table that holds the items Northwind sells is the Products table.
+b) The table that holds the types or categories of the items Northwind sells is the Categories table.
+*/
+
+USE northwind;
+
+SELECT *
+FROM Employees;
+
+
