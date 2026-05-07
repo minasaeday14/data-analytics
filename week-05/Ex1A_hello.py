f6af5b9d4dd36d2e4c50b3f66b689eab77164880
Mina Saeday <minaseid14@gmail.com>
2026-05-07 13:28:47 -0500

    updated Ex1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,13 @@
 print('Hello World!')
+
+dollars = 3
+cents = 0.50
+print(dollars+cents)
+#the output was 3.5 it ignored the 0
+
+cents = cents + 0.25
+print(cents)
+d_str = '3 dollars'
+
+c_str = '50 cents'
+print(d_str + ' and ' + c_str) # the output was printed as a string
