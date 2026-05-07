557c5a89349f5db3c01c47b87e77051098de29af
Mina Saeday <minaseid14@gmail.com>
2026-05-07 13:53:39 -0500

    EX1B DONE

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+customer_id = 1001 # I assumed customer IDs are numbers, but in some systems they could also be text.
+customer_name = "Mina Saeday"  # I wrote the full name in one variable, but first and last name could also be stored separately.
+customer_gender = "Female" # stored it as a string
+customer_date_of_birth = "2004-05-22" # stored it as a string
+auto_policy_number = "AUTO-987654"  # I assumed policy numbers are text since they often contain letters, numbers, or symbols.
+my_name = "Mina Saeday" # I used my full name as a string
+city_and_state = "Addis Ababa, Ethiopia"  # I combined city and state in one variable, but they could also be split into two variables.
+
+
+# Lab 2 answers
+
+# a) Full list of Python reserved words / keywords:
+# False, None, True, and, as, assert, async, await, break, class, continue,
+# def, del, elif, else, except, finally, for, from, global, if, import, in,
+# is, lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+# b) Five keyword definitions:
+# if: used to run code only when a condition is true.
+# def: used to define a ^^function^^ in Python.
+# return: used to send a value back from a function.
+# try: used to test code that might cause an ^^exception^^.
+# global: used to tell Python that a variable belongs to the global ^^scope^^, not just the function.
+
