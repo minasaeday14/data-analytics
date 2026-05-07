89c0e9e0e156c29d806c72c3a264105a5097fe48
Mina Saeday <minaseid14@gmail.com>
2026-05-07 15:42:55 -0500

    Lab4

--- week-05/Ex2A_MathScripts/area_of_rectangle.py
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -1,9 +1,8 @@
-side_a = float(input("What is the length of the first side?"))
-side_b = float(input("What is the length of the second side?"))
-
+side_a = 5
+side_b = 14
 area = side_a * side_b
-print("Side A is", str(side_a))
-print("Side B is ", str(side_b))
-print("The area of the rectangle is ", area)
+print(f"Side A is {side_a}")
+print(f"Side B is {side_b}")
+print(f"The area of the rectangle is {area} " )
 
 
