daabf47084139ea855a504dd504a60167f01e191
Mina Saeday <minaseid14@gmail.com>
2026-05-08 13:03:47 -0500

    distance.py

---
+++ week-05/Ex2B_MoreMathScripts/distance.py
@@ -0,0 +1,10 @@
+import math
+
+x1 = 2
+y1 = 3
+x2 = 8
+y2 = 11
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"The distance between ({x1}, {y1}) and ({x2}, {y2}) is {distance:.2f} ")
