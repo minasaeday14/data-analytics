559593ed4748781d89c055fde9cf50be494b142a
Mina Saeday <minaseid14@gmail.com>
2026-05-08 23:25:13 -0500

    Create  bonus_gregorian_calendar.py

---
+++ week-05/Ex4A_IfScripts./ bonus_gregorian_calendar.py
@@ -0,0 +1,10 @@
+year = 2000
+
+if year % 400 == 0:
+    print(year, "is a leap year")
+elif year % 100 == 0:
+    print(year, "is not a leap year")
+elif year % 4 == 0:
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
